719aca17fbebc6b9a54fc5e5c8c1a63e0260531f
dcarreras <dca.ingedif@gmail.com>
2024-06-29 10:26:47 +0200

    feat 3.4. Using custom types for the Project class
    Using custom types for the Project class

---
+++ 02-Creating Interactive Websites/src/class/Project.ts
@@ -0,0 +1,27 @@
+export type ProjectStatus = "pending" | "active" | "finished"
+export type UserRole = "architect" | "engineer" | "developer"
+
+export interface  IProject {
+    name: string;
+    description: string;
+    status: ProjectStatus;
+    userRole: UserRole;
+    finishDate: Date;
+}
+
+export class Project implements IProject {
+    name: string;
+    description: string;
+    status: "pending" | "active" | "finished";
+    userRole: "architect" | "engineer" | "developer";
+    finishDate: Date;
+  
+    constructor(data: IProject) {
+      this.name = data.name;
+      this.description = data.description;
+      this.status = data.status;
+      this.userRole = data.userRole;
+      this.finishDate = data.finishDate;
+    }
+  }
+  
--- 02-Creating Interactive Websites/src/index.ts
+++ 02-Creating Interactive Websites/src/index.ts
@@ -1,4 +1,4 @@
-import { Project } from "./class/Project";
+import { Project, IProject, UserRole, ProjectStatus} from "./class/Project";
 
 function showModal(id: string) {
   const modal = document.getElementById(id);
@@ -24,12 +24,12 @@ if (projectForm) {
   projectForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(projectForm);
-    const projectData = {
-      name: formData.get("name"),
-      description: formData.get("description"),
-      status: formData.get("status"),
-      userRole: formData.get("userRole"),
-      finishDate: formData.get("finishDate"),
+    const projectData : IProject = {
+      name: formData.get("name") as string,
+      description: formData.get("description")as string,
+      status: formData.get("status") as ProjectStatus,
+      userRole: formData.get("userRole") as UserRole,
+      finishDate: new Date(formData.get("finishDate")as string),
     };
     const project = new Project(projectData);
     console.log(project);
