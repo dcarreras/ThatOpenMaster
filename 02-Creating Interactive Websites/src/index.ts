eec2514b41dae17330ca3941c6b07d5710f3f2da
dcarreras <dca.ingedif@gmail.com>
2024-06-30 10:37:46 +0200

    Auto stash before checking out "HEAD"

--- 02-Creating Interactive Websites/src/index.ts
+++ 02-Creating Interactive Websites/src/index.ts
@@ -1,13 +1,26 @@
-import { Project, IProject, UserRole, ProjectStatus} from "./class/Project";
+import { Project, IProject, UserRole, ProjectStatus } from "./class/Project";
+import { ProjectsManager } from "./class/ProjectsManager";
 
 function showModal(id: string) {
   const modal = document.getElementById(id);
   if (modal && modal instanceof HTMLDialogElement) {
     modal.showModal();
   } else {
-    console.warn("The provided modal wasn't found. ID: ", id);
+    console.warn("The provided modal wasn't found. ID:", id);
   }
 }
+
+function closeModal(id: string) {
+  const modal = document.getElementById(id);
+  if (modal && modal instanceof HTMLDialogElement) {
+    modal.close();
+  } else {
+    console.warn("The provided modal wasn't found. ID:", id);
+  }
+}
+
+const projectListUI = document.getElementById("projects-list") as HTMLElement;
+const projectsManager = new ProjectsManager(projectListUI);
 
 const newProjectBtn = document.getElementById("new-project-btn");
 if (newProjectBtn) {
@@ -18,23 +31,25 @@ if (newProjectBtn) {
   console.warn("new-project-btn was not found");
 }
 
-const projectForm = document.getElementById('formId') as HTMLFormElement;
+const projectForm = document.getElementById('new-project-form') as HTMLFormElement;
 
 if (projectForm) {
   projectForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(projectForm);
-    const projectData : IProject = {
+    const projectData: IProject = {
       name: formData.get("name") as string,
-      description: formData.get("description")as string,
+      description: formData.get("description") as string,
       status: formData.get("status") as ProjectStatus,
       userRole: formData.get("userRole") as UserRole,
-      finishDate: new Date(formData.get("finishDate")as string),
+      finishDate: new Date(formData.get("finishDate") as string),
+      cost: parseFloat(formData.get("cost") as string),
+      progress: parseInt(formData.get("progress") as string)
     };
-    const project = new Project(projectData);
-    console.log(project);
+    const project = projectsManager.newProject(projectData);
+    projectForm.reset();
+    closeModal("new-project-modal");
   });
 } else {
   console.warn("new-project-form was not found. Please check the id");
 }
-
--- 02-Creating Interactive Websites/vite.config.js
+++ 02-Creating Interactive Websites/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Configuración aquí
+  server: {
+    open: true,
+  },
+  // Otras configuraciones
 });
