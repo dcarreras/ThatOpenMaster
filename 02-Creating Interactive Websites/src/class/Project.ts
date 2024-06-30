038174077e374a254d54bd0e03dce2a08f7a95a2
dcarreras <dca.ingedif@gmail.com>
2024-06-30 09:17:51 +0200

    feat 3.5 Creating projects cards from code
    Implementation of project UI with dynamic content using template literals. Improved readability and introduced methods to prevent UI duplication and set default values for unspecified properties.

--- 02-Creating Interactive Websites/src/class/Project.ts
+++ 02-Creating Interactive Websites/src/class/Project.ts
@@ -1,28 +1,67 @@
-export type ProjectStatus = "pending" | "active" | "finished"
-export type UserRole = "architect" | "engineer" | "developer"
+export type ProjectStatus = "pending" | "active" | "finished";
+export type UserRole = "architect" | "engineer" | "developer";
 
-export interface  IProject {
+export interface IProject {
     name: string;
     description: string;
     status: ProjectStatus;
     userRole: UserRole;
     finishDate: Date;
+    cost: number;
+    progress: number;
 }
 
 export class Project implements IProject {
     name: string;
     description: string;
-    status: "pending" | "active" | "finished";
-    userRole: "architect" | "engineer" | "developer";
+    status: ProjectStatus;
+    userRole: UserRole;
     finishDate: Date;
-  
+    cost: number;
+    progress: number;
+    ui: HTMLDivElement;
+
     constructor(data: IProject) {
-      this.name = data.name;
-      this.description = data.description;
-      this.status = data.status;
-      this.userRole = data.userRole;
-      this.finishDate = data.finishDate;
-      const card = document.createElement("div")
+        this.name = data.name;
+        this.description = data.description;
+        this.status = data.status;
+        this.userRole = data.userRole;
+        this.finishDate = data.finishDate;
+        this.cost = data.cost;
+        this.progress = data.progress;
+        this.setUI();
     }
-  }
-  
+
+    setUI() {  
+        if(this.ui){return}
+        this.ui = document.createElement("div");
+        this.ui.className = "project-card";
+        this.ui.innerHTML = `
+            <div class="card-header">
+                <p style="background-color: #ca8134; padding: 10px; border-radius: 8px;">PC1</p>
+                <div>
+                    <h5>${this.name}</h5>
+                    <p>${this.description}</p>
+                </div>
+            </div>
+            <div class="card-content">
+                <div class="card-property">
+                    <p style="color: #969696;">Status</p>
+                    <p>${this.status}</p>
+                </div>
+                <div class="card-property">
+                    <p style="color: #969696;">Role</p>
+                    <p>${this.userRole}</p>
+                </div>
+                <div class="card-property">
+                    <p style="color: #969696;">Cost</p>
+                    <p>${this.cost}</p>
+                </div>
+                <div class="card-property">
+                    <p style="color: #969696;">Progress</p>
+                    <p>${this.progress}%</p>
+                </div>
+            </div>
+        `;
+    }
+}
