d3d2031c5f66e9a2f6929f0243c924798c4092de
dcarreras <40931486+dcarreras@users.noreply.github.com>
2026-01-10 10:00:18 +0100

    Reorder appIcons properties for better readability

--- 03-Creating BIM apps/src/globals.ts
+++ 03-Creating BIM apps/src/globals.ts
@@ -1,7 +1,7 @@
 export const appIcons = {
-    status: "fluent:status-12-filled",
-    role: "fluent:person-20-regular",
     cost: "fluent:money-20-regular",
     progress: "fluent:chart-multiple-20-regular",
-    search: "fluent:search-20-regular"
+    role: "fluent:person-20-regular",
+    search: "fluent:search-20-regular",
+    status: "fluent:status-12-filled"
 };
